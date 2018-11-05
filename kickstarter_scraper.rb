c3dd0b194da0f4817a7e9327800dac7ae5971d5e
Kaela Clement <stephanie.kaela@gmail.com>
2018-11-05 05:44:22 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,6 +7,11 @@ kickstarter = Nokogiri::HTML(html)
 
 # project: kickstarter.css("li.project.grid_4")
 # title: project.css("h2.bbcard_name strong a").text
+# image: project.css("div.project-thumbnail a img").attribute("src").value
+#        project.css("img.projectphoto-little").attribute("src").value
+# description: project.css("p.bbcard_blurb").text
+# location: project.css("span.location-name").text
+# percent funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
 
 def create_project_hash
   # write your code here
